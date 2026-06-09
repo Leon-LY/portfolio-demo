3827ba20f19babf373d68360221d8f6017ebc1b9
Leon-LY <554295000@qq.com>
2026-06-09 10:40:47 +0800

    fix: scroll to top on every route change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 export default function Layout() {
   return (
     <div className="min-h-screen bg-[#0a0e1a] text-[#e2e8f0]">
+      <ScrollToTop />
       <Navbar />
       <main>
         <AnimatePresence mode="wait">
